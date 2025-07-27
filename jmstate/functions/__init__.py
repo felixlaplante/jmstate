f6c68e137dbd5678f1c5458f8bf0f31c5e79ce40
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-07-27 22:25:45 +0200

    Delete jmstate directory

--- jmstate/functions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_hazards import exponential, gompertz, log_normal, weibull
-
-__all__ = ["exponential", "gompertz", "log_normal", "weibull"]
--- jmstate/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model import MultiStateJointModel
-
-__all__ = ["MultiStateJointModel"]
