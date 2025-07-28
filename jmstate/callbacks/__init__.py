77137c442b159edd7946ef286edc3e017633352e
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-07-28 13:03:54 +0200

    Delete jmstate directory

--- jmstate/callbacks/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._computations import compute_criteria, compute_ebes, compute_fim  # noqa: D104
-
-__all__ = ["compute_criteria", "compute_ebes", "compute_fim"]
--- jmstate/functions/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._base_hazards import exponential, gompertz, log_normal, weibull  # noqa: D104
-
-__all__ = ["exponential", "gompertz", "log_normal", "weibull"]
