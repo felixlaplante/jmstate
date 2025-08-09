f19fa31a78bb9c84926730a50e138990c0f6c7d4
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-08-09 11:49:35 +0200

    Update _individual_effects.py

--- jmstate/functions/_individual_effects.py
+++ jmstate/functions/_individual_effects.py
@@ -3,6 +3,22 @@ import torch
 from ..typedefs._defs import Tensor2D, Tensor3D
 
 
+def identity(
+    gamma: torch.Tensor, x: Tensor2D, b: Tensor2D | Tensor3D
+) -> Tensor2D | Tensor3D:
+    """The standard linear transformation b.
+
+    Args:
+        gamma (torch.Tensor): The population parameters.
+        x (Tensor2D): The (fixed) covariates.
+        b (Tensor2D | Tensor3D): The random effects.
+
+    Returns:
+        Tensor2D | Tensor3D: The identity b
+    """
+    return b
+
+
 def gamma_x_plus_b(
     gamma: torch.Tensor, x: Tensor2D, b: Tensor2D | Tensor3D
 ) -> Tensor2D | Tensor3D:
