3bf3694014ff29fd8b0607bfc24071caa8f77e97
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-09-13 21:51:07 +0200

    Update _individual_effects.py

--- jmstate/functions/_individual_effects.py
+++ jmstate/functions/_individual_effects.py
@@ -1,7 +1,13 @@
+from typing import cast
+
 import torch
 
 
-def identity(gamma: torch.Tensor, x: torch.Tensor, b: torch.Tensor) -> torch.Tensor:  # noqa: ARG001
+def identity(
+    gamma: torch.Tensor | None,  # noqa: ARG001
+    x: torch.Tensor | None,  # noqa: ARG001
+    b: torch.Tensor,
+) -> torch.Tensor:
     r"""The standard identity transformation.
 
     It is useful when only random effects are considered as individual parameters.
@@ -11,8 +17,8 @@ def identity(gamma: torch.Tensor, x: torch.Tensor, b: torch.Tensor) -> torch.Ten
         (\gamma, x, b) \mapsto b.
 
     Args:
-        gamma (torch.Tensor): The population parameters.
-        x (torch.Tensor): The (fixed) covariates.
+        gamma (torch.Tensor | None): The population parameters.
+        x (torch.Tensor | None): The (fixed) covariates.
         b (torch.Tensor): The random effects.
 
     Returns:
@@ -22,7 +28,7 @@ def identity(gamma: torch.Tensor, x: torch.Tensor, b: torch.Tensor) -> torch.Ten
 
 
 def gamma_x_plus_b(
-    gamma: torch.Tensor, x: torch.Tensor, b: torch.Tensor
+    gamma: torch.Tensor | None, x: torch.Tensor | None, b: torch.Tensor
 ) -> torch.Tensor:
     r"""The standard linear transformation.
 
@@ -34,19 +40,19 @@ def gamma_x_plus_b(
         (\gamma, x, b) \mapsto \gamma x + b
 
     Args:
-        gamma (torch.Tensor): The population parameters.
-        x (torch.Tensor): The (fixed) covariates.
+        gamma (torch.Tensor | None): The population parameters. It must not be None.
+        x (torch.Tensor | None): The (fixed) covariates. It must not be None.
         b (torch.Tensor): The random effects.
 
     Returns:
         torch.Tensor: The transformation x @ gamma + b
     """
-    return x @ gamma + b
+    return cast(torch.Tensor, x) @ cast(torch.Tensor, gamma) + b
 
 
 def gamma_plus_b(
-    gamma: torch.Tensor,
-    x: torch.Tensor,  # noqa: ARG001
+    gamma: torch.Tensor | None,
+    x: torch.Tensor | None,  # noqa: ARG001
     b: torch.Tensor,
 ) -> torch.Tensor:
     r"""The standard linear transformation.
@@ -59,11 +65,11 @@ def gamma_plus_b(
         (\gamma, x, b) \mapsto \gamma + b
 
     Args:
-        gamma (torch.Tensor): The population parameters.
-        x (torch.Tensor): The (fixed) covariates.
+        gamma (torch.Tensor | None): The population parameters. It must not be None.
+        x (torch.Tensor | None): The (fixed) covariates.
         b (torch.Tensor): The random effects.
 
     Returns:
         torch.Tensor: The transformation gamma + b
     """
-    return gamma + b
+    return cast(torch.Tensor, gamma) + b
