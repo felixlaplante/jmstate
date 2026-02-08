49767b162222a767bf82382fc4cf3a7c096ed658
Félix Laplante <75139846+felixlaplante0@users.noreply.github.com>
2026-02-08 22:00:10 +0100

    Add _static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ napoleon_attr_annotations = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = []
+html_static_path = ["_static"]
 
 
 
