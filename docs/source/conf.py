f7ffab9f13b1d4ad3ea4dcdbf833945316a9a7a4
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2026-02-08 19:52:15 +0100

    Try to fix pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -39,4 +44,5 @@ napoleon_attr_annotations = True
 
 html_theme = "furo"
 html_static_path = ["_static"]
+
 
