d160078168dab9e2d16746dd5caf85dfb2c672c0
Félix Laplante <75139846+felixlaplante0@users.noreply.github.com>
2026-02-10 23:45:39 +0100

    Remove license from init

--- jmstate/__init__.py
+++ jmstate/__init__.py
@@ -5,8 +5,4 @@ This package provides tools for multistate joint modeling with PyTorch.
 
 from .model import MultiStateJointModel
 
-__author__ = "Félix Laplante"
-__email__ = "math.felixlaplante@proton.me"
-__license__ = "MIT"
-
 __all__ = ["MultiStateJointModel"]
